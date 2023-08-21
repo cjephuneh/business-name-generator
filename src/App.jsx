2f931cd2d51c6b54dfc9874abdd85ab0dcf3f5b0
cjephuneh <calebjephuneh@gmail.com>
2023-08-21 18:19:50 +0300

    tailwind_config

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-red-400'>App</div>
+    <div className='bg-red-400 font-bold underline'>App.config.js</div>
   )
 }
 
